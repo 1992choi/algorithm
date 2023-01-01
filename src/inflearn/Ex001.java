075b0ab40876b7b8546e10e9d85a70a651d18d8a
choi <younggeunn@naver.com>
2023-01-01 13:49:32 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 1. 문자 찾기

--- src/inflearn/Ex001.java
+++ src/inflearn/Ex001.java
@@ -10,12 +10,26 @@ public class Ex001 {
         char ch = in.nextLine().charAt(0);
 
         System.out.println(solution(str, ch));
+        System.out.println(solution2(str, ch));
     }
 
     public static int solution(String str, char ch) {
         int answer = 0;
         for (int i = 0; i < str.length(); i++) {
             if (String.valueOf(str.charAt(i)).equalsIgnoreCase(String.valueOf(ch))) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static int solution2(String str, char ch) {
+        int answer = 0;
+        str = str.toUpperCase();
+        ch = Character.toUpperCase(ch);
+        for (char c : str.toCharArray()) {
+            if (c == ch) {
                 answer++;
             }
         }
