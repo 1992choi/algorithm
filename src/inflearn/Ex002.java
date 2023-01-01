5fe8d51a9953bbb8ed1f5a8d950c5ae0583c32e4
choi <younggeunn@naver.com>
2023-01-01 14:16:14 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 2. 대소문자 변환

---
+++ src/inflearn/Ex002.java
@@ -0,0 +1,27 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Ex002 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+
+        System.out.println(solution(str));
+    }
+
+    public static String solution(String str) {
+        String answer = "";
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                answer += Character.toUpperCase(c);
+            } else {
+                answer += Character.toLowerCase(c);
+            }
+        }
+
+        return answer;
+    }
+
+}
