dbd6f5edf897d285f908e1aa74c4911db5a47467
choi <younggeunn@naver.com>
2023-01-03 20:59:09 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 4. 단어 뒤집기(StringBuilder이용법 또는 직접뒤집기)

--- src/inflearn/Ex004.java
+++ src/inflearn/Ex004.java
@@ -14,13 +14,39 @@ public class Ex004 {
         }
 
         solution(num, arr);
+        solution2(num, arr);
+        solution3(num, arr);
     }
 
     public static void solution(int num, String[] arr) {
         for (int i = 0; i < num; i++) {
-            StringBuffer sb = new StringBuffer();
-            sb.append(arr[i]);
+            StringBuffer sb = new StringBuffer(arr[i]);
             System.out.println(sb.reverse());
+        }
+    }
+
+    public static void solution2(int num, String[] arr) {
+        for (int i = 0; i < num; i++) {
+            StringBuilder sb = new StringBuilder(arr[i]);
+            System.out.println(sb.reverse());
+        }
+    }
+
+    public static void solution3(int num, String[] arr) {
+        for (int i = 0; i < num; i++) {
+            int lt = 0;
+            int rt = arr[i].length() - 1;
+
+            char[] ch = arr[i].toCharArray();
+            while (lt < rt) {
+                char tmp = ch[lt];
+                ch[lt] = ch[rt];
+                ch[rt] = tmp;
+                lt++;
+                rt--;
+            }
+
+            System.out.println(String.valueOf(ch));
         }
     }
 
