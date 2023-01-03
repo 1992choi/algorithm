94a3123f1aa61afc841ae7496b53cde040a65686
choi <younggeunn@naver.com>
2023-01-03 21:39:17 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 6. 중복문자제거

--- src/inflearn/Ex006.java
+++ src/inflearn/Ex006.java
@@ -9,6 +9,7 @@ public class Ex006 {
         String str = in.nextLine();
 
         System.out.println(solution(str));
+        System.out.println(solution2(str));
     }
 
     public static String solution(String str) {
@@ -23,6 +24,24 @@ public class Ex006 {
 
             answer += String.valueOf(ch);
             set.add(ch);
+        }
+
+        return answer;
+    }
+
+    public static String solution2(String str) {
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            /*
+                i는 현재 문자의 위치,
+                str.indexOf(str.charAt(i))는 해당 문자가 최초로 발견된 위치
+
+                현재 문자의 위치와 최초로 발견된 위치가 다르다면, 중복된 문자라는 뜻
+             */
+            if (i == str.indexOf(str.charAt(i))) {
+                answer += str.charAt(i);
+            }
         }
 
         return answer;
