bb4743952e9f36d89bf9706c4fd557dc71ad2da2
choi <younggeunn@naver.com>
2023-01-04 20:44:19 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 8. 유효한 팰린드롬

--- src/inflearn/Ex008.java
+++ src/inflearn/Ex008.java
@@ -9,6 +9,7 @@ public class Ex008 {
         String str = in.nextLine();
 
         System.out.println(solution(str));
+        System.out.println(solution2(str));
     }
 
     public static String solution(String str) {
@@ -16,6 +17,17 @@ public class Ex008 {
         String reverseStr = new StringBuilder(str).reverse().toString();
 
         if (str.equalsIgnoreCase(reverseStr)) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    public static String solution2(String str) {
+        str = str.toUpperCase().replaceAll("[^A-Z]","");
+        String reverseStr = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(reverseStr)) {
             return "YES";
         } else {
             return "NO";
