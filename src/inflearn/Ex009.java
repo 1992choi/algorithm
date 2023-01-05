480672d3fb88a91dbf925eab6901f733b70332af
choi <younggeunn@naver.com>
2023-01-05 21:22:38 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 9. 숫자만 추출

--- src/inflearn/Ex009.java
+++ src/inflearn/Ex009.java
@@ -9,10 +9,34 @@ public class Ex009 {
         String str = in.nextLine();
 
         System.out.println(solution(str));
+        System.out.println(solution2(str));
+        System.out.println(solution3(str));
     }
 
     public static int solution(String str) {
         return Integer.parseInt(str.replaceAll("[^0-9]",""));
+    }
+
+    public static int solution2(String str) {
+        int answer = 0;
+        for (char x : str.toCharArray()) {
+            if (x >= 48 && x <= 57) {
+                answer = answer * 10 + (x - 48);
+            }
+        }
+
+        return answer;
+    }
+
+    public static int solution3(String str) {
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) {
+                answer += x;
+            }
+        }
+
+        return Integer.parseInt(answer);
     }
 
 }
