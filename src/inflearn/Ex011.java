816ab2a2b4541f6c490d1da01f9a49651fdadce6
choi <younggeunn@naver.com>
2023-01-06 21:15:13 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 11. 문자열 압축

--- src/inflearn/Ex011.java
+++ src/inflearn/Ex011.java
@@ -9,6 +9,7 @@ public class Ex011 {
         String str = in.nextLine();
 
         System.out.println(solution(str));
+        System.out.println(solution2(str));
     }
 
     public static String solution(String str) {
@@ -26,6 +27,27 @@ public class Ex011 {
                     answer += count;
                 }
                 prevChar = str.charAt(i);
+                count = 1;
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static String solution2(String str) {
+        str += ";";
+        String answer = "";
+
+        int count = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                answer += str.charAt(i);
+                if (count != 1) {
+                    answer += count;
+                }
                 count = 1;
             }
 
