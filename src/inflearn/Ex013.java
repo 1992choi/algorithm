d89310f4802997375a4487a4e973ac39952d69cb
choi <younggeunn@naver.com>
2023-01-07 10:19:56 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Array(1, 2차원 배열) > 1. 큰 수 출력하기

---
+++ src/inflearn/Ex013.java
@@ -0,0 +1,34 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Ex013 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+        int[] nums = new int[count];
+        for (int i = 0; i < count; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        System.out.println(solution(count, nums));
+    }
+
+    public static String solution(int count, int[] nums) {
+        String answer = "";
+        for (int i = 0; i < count; i++) {
+            if (i == 0) {
+                answer += nums[0] + " ";
+                continue;
+            }
+
+            if (nums[i - 1] < nums[i]) {
+                answer += nums[i] + " ";
+            }
+        }
+
+        return answer;
+    }
+
+}
