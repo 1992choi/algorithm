bbd6fe4b9773ea6296cbd6db1630631a9d3f601d
choi <younggeunn@naver.com>
2023-01-09 21:28:23 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Array(1, 2차원 배열) > 5. 소수

---
+++ src/inflearn/Ex017.java
@@ -0,0 +1,33 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Ex017 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        System.out.println(solution(num));
+    }
+
+    public static int solution(int num) {
+        int answer = 0;
+
+        for (int i = 1; i <= num; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+}
