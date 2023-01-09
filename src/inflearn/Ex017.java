dfcc27f026415186bf4e9aa9e160f9d7144e7188
choi <younggeunn@naver.com>
2023-01-09 21:39:15 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Array(1, 2차원 배열) > 5. 소수(에라토스테네스 체)

--- src/inflearn/Ex017.java
+++ src/inflearn/Ex017.java
@@ -9,6 +9,7 @@ public class Ex017 {
         int num = in.nextInt();
 
         System.out.println(solution(num));
+        System.out.println(solution2(num));
     }
 
     public static int solution(int num) {
@@ -24,6 +25,27 @@ public class Ex017 {
 
             if (count == 2) {
                 answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static int solution2(int num) {
+        int answer = 0;
+
+        int[] arr = new int[num + 1];
+        arr[0] = 1; // 0은 판단에서 제외
+        arr[1] = 1; // 1은 판단에서 제외
+
+        for (int i = 2; i <= num; i++) {
+            if (arr[i] == 0) {
+                answer++;
+
+                // 배수는 모두 1로 만들어서 다음 조건에 만족하지 않도록 만들기
+                for (int j = i; j <= num; j += i) {
+                    arr[j] = 1;
+                }
             }
         }
 
