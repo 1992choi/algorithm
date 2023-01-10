ed8df3c95ace403e33020b5eb6b47993f850ac21
choi <younggeunn@naver.com>
2023-01-10 22:45:38 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Array(1, 2차원 배열) > 8. 등수구하기

---
+++ src/inflearn/Ex020.java
@@ -0,0 +1,49 @@
+package inflearn;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ex020 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+        Integer[] nums = new Integer[count];
+
+        for (int i = 0; i < count; i++) {
+            nums[i] = in.nextInt();
+        }
+
+        solution(count, nums);
+    }
+
+    public static void solution(int count, Integer[] nums) {
+        Integer[] ranks = new Integer[count];
+        Integer[] sortedNums = Arrays.copyOf(nums, count);
+        Arrays.sort(sortedNums, Collections.reverseOrder());
+
+        int currentRank = 1; // 현재 등수
+        int cumulativeNum = 1; // 동률 수
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                if (sortedNums[i] == nums[j]) {
+                    ranks[j] = currentRank;
+                    cumulativeNum++;
+
+                    /*
+                        등수가 배정된 학생은 점수를 음수로 변경.
+                        -> 점수가 동일한 경우가 있는 경우, 등수가 재배정될 수 있음.
+                     */
+                    nums[j] = -1;
+                }
+            }
+            currentRank = cumulativeNum;
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.print(ranks[i] + " ");
+        }
+    }
+
+}
