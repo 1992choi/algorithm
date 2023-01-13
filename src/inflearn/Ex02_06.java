8c259e3dc39a725e79c76fb0929fb5b6bc4daa2c
choi <younggeunn@naver.com>
2023-01-13 22:10:46 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Array(1, 2차원 배열) > 6. 뒤집은 소수

--- src/inflearn/Ex02_06.java
+++ src/inflearn/Ex02_06.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class Ex018 {
+public class Ex02_06 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -16,8 +16,7 @@ public class Ex018 {
             nums[i] = in.nextInt();
         }
 
-        // solution(count, nums);
-        solution2(count, nums);
+        solution(count, nums);
     }
 
     public static void solution(int count, int[] nums) {
@@ -50,37 +49,6 @@ public class Ex018 {
                 System.out.print(nums[i] + " ");
             }
         }
-    }
-
-    public static void solution2(int count, int[] nums) {
-        // 자연수 뒤집기
-        for (int i = 0; i < count; i++) {
-            int tmp = nums[i];
-            int res = 0;
-            while (tmp > 0) {
-                int t = tmp % 10;
-                res = res * 10 + t;
-                tmp = tmp / 10;
-            }
-
-            if (isPrime(res)) {
-                System.out.print(res + " ");
-            }
-        }
-    }
-
-    public static boolean isPrime(int num) {
-        if (num == 1) {
-            return false;
-        }
-
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
 }
