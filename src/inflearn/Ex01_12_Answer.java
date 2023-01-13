bd1863dfd15117129b731b56549f828de17ec005
choi <younggeunn@naver.com>
2023-01-13 22:01:51 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 12. 암호

--- src/inflearn/Ex01_12_Answer.java
+++ src/inflearn/Ex01_12_Answer.java
@@ -2,7 +2,7 @@ package inflearn;
 
 import java.util.Scanner;
 
-public class Ex012 {
+public class Ex01_12_Answer {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -10,22 +10,9 @@ public class Ex012 {
         String str = in.next();
 
         System.out.println(solution(num, str));
-        System.out.println(solution2(num, str));
     }
 
     public static String solution(int num, String str) {
-        String answer = "";
-        int blockNum = str.length() / num;
-        str = str.replaceAll("#", "1").replaceAll("\\*", "0");
-
-        for (int i = 0; i < num; i++) {
-            answer += (char)(Integer.parseInt(str.substring((blockNum * i), (blockNum * (i + 1))), 2));
-        }
-
-        return answer;
-    }
-
-    public static String solution2(int num, String str) {
         String answer = "";
         int blockNum = str.length() / num;
         for (int i = 0; i < num; i++) {
