86baea534cc69e1673f0fd6e2b0295c8f0cb2b42
choi <younggeunn@naver.com>
2023-01-13 22:15:47 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Array(1, 2차원 배열) > 9. 격자판 최대합

--- src/inflearn/Ex02_09.java
+++ src/inflearn/Ex02_09.java
@@ -2,7 +2,7 @@ package inflearn;
 
 import java.util.Scanner;
 
-public class Ex021 {
+public class Ex02_09 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -16,7 +16,6 @@ public class Ex021 {
         }
 
         System.out.println(solution(count, arr));
-        System.out.println(solution2(count, arr));
     }
 
     public static int solution(int count, int[][] arr) {
@@ -65,37 +64,6 @@ public class Ex021 {
         if (sum > maxSum) {
             maxSum = sum;
         }
-
-        return maxSum;
-    }
-
-    public static int solution2(int count, int[][] arr) {
-        int maxSum = Integer.MIN_VALUE;
-
-        int sum1 = 0; // 행의 합
-        int sum2 = 0; // 열의 합
-
-        // 가로, 세로 판단
-        for (int i = 0; i < count; i++) {
-            sum1 = 0;
-            sum2 = 0;
-            for (int j = 0; j < count; j++) {
-                sum1 += arr[i][j];
-                sum2 += arr[j][i];
-            }
-            maxSum = Math.max(maxSum, sum1);
-            maxSum = Math.max(maxSum, sum2);
-        }
-
-        // 대각선 판단
-        sum1 = 0; // 행의 합
-        sum2 = 0; // 열의 합
-        for (int i = 0; i < count; i++) {
-            sum1 += arr[i][i];
-            sum2 += arr[i][count - i - 1];
-        }
-        maxSum = Math.max(maxSum, sum1);
-        maxSum = Math.max(maxSum, sum2);
 
         return maxSum;
     }
