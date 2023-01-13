0c7b908f29cddeac20b08856b318925d35930958
choi <younggeunn@naver.com>
2023-01-13 21:50:14 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 5. 특정 문자 뒤집기

--- src/inflearn/Ex01_05_Answer.java
+++ src/inflearn/Ex01_05_Answer.java
@@ -2,39 +2,16 @@ package inflearn;
 
 import java.util.Scanner;
 
-public class Ex005 {
+public class Ex01_05_Answer {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
 
         System.out.println(solution(str));
-        System.out.println(solution2(str));
     }
 
     public static String solution(String str) {
-        int lt = 0;
-        int rt = str.length() - 1;
-
-        char[] ch = str.toCharArray();
-        while (lt < rt) {
-            if (!Character.isAlphabetic(ch[lt])) {
-                lt++;
-            } else if (!Character.isAlphabetic(ch[rt])) {
-                rt--;
-            } else {
-                char tmp = ch[lt];
-                ch[lt] = ch[rt];
-                ch[rt] = tmp;
-                lt++;
-                rt--;
-            }
-        }
-
-        return String.valueOf(ch);
-    }
-
-    public static String solution2(String str) {
         String answer;
         char[] ch = str.toCharArray();
         int lt = 0;
