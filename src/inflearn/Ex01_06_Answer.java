344f498db0172fabbe5f54ec46544dd98e3b4b7f
choi <younggeunn@naver.com>
2023-01-13 21:51:38 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 6. 중복문자제거

--- src/inflearn/Ex01_06_Answer.java
+++ src/inflearn/Ex01_06_Answer.java
@@ -1,35 +1,17 @@
 package inflearn;
 
-import java.util.*;
+import java.util.Scanner;
 
-public class Ex006 {
+public class Ex01_06_Answer {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
 
         System.out.println(solution(str));
-        System.out.println(solution2(str));
     }
 
     public static String solution(String str) {
-        String answer = "";
-        Set<Character> set = new HashSet<>();
-
-        char[] chars = str.toCharArray();
-        for (char ch : chars) {
-            if (set.contains(ch)) {
-                continue;
-            }
-
-            answer += String.valueOf(ch);
-            set.add(ch);
-        }
-
-        return answer;
-    }
-
-    public static String solution2(String str) {
         String answer = "";
 
         for (int i = 0; i < str.length(); i++) {
