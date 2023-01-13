20ce17bf0fb8db0807bec5db351ed19ced421547
choi <younggeunn@naver.com>
2023-01-13 21:47:10 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 3. 문장 속 단어

--- src/inflearn/Ex01_03.java
+++ src/inflearn/Ex01_03.java
@@ -2,14 +2,13 @@ package inflearn;
 
 import java.util.Scanner;
 
-public class Ex003 {
+public class Ex01_03 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
 
         System.out.println(solution(str));
-        System.out.println(solution2(str));
     }
 
     public static String solution(String str) {
@@ -21,27 +20,6 @@ public class Ex003 {
                 max = s.length();
                 answer = s;
             }
-        }
-
-        return answer;
-    }
-
-    public static String solution2(String str) {
-        String answer = "";
-        int max = 0;
-        int pos;
-        while ((pos = str.indexOf(" ")) != -1) {
-            String tmp = str.substring(0, pos);
-            if (tmp.length() > max) {
-                max = tmp.length();
-                answer = tmp;
-            }
-
-            str = str.substring(pos + 1);
-        }
-
-        if (str.length() > max) {
-            answer = str;
         }
 
         return answer;
