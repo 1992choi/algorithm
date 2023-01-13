d8757ab429d4d7086d71f51a14aee68a4c5e9e8f
choi <younggeunn@naver.com>
2023-01-13 22:08:38 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Array(1, 2차원 배열) > 4. 피보나치 수열

--- src/inflearn/Ex02_04.java
+++ src/inflearn/Ex02_04.java
@@ -2,15 +2,13 @@ package inflearn;
 
 import java.util.Scanner;
 
-public class Ex016 {
+public class Ex02_04 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
 
         solution(num);
-        System.out.println();
-        solution2(num);
     }
 
     public static void solution(int num) {
@@ -23,20 +21,6 @@ public class Ex016 {
 
         for (int i = 0; i < num; i++) {
             System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static void solution2(int num) {
-        int a = 1;
-        int b = 1;
-        int c;
-
-        System.out.print(a + " " + b + " ");
-        for (int i = 2; i < num; i++) {
-            c = a + b;
-            System.out.print(c + " ");
-            a = b;
-            b = c;
         }
     }
 
---
+++ src/inflearn/Ex02_04_Answer.java
@@ -0,0 +1,28 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Ex02_04_Answer {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        solution(num);
+    }
+
+    public static void solution(int num) {
+        int a = 1;
+        int b = 1;
+        int c;
+
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < num; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+
+}
