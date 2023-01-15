8fab0ce5f39188739f78aa75fd438b2d72428504
choi <younggeunn@naver.com>
2023-01-15 11:34:36 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Two pointers, Sliding window > 4. 연속 부분수열

---
+++ src/inflearn/Ex03_04.java
@@ -0,0 +1,47 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Ex03_04 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(solution(n, m, arr));
+    }
+
+    public static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p2 < n) {
+            int sum = 0;
+            for (int i = p1; i <= p2; i++) {
+                sum += arr[i];
+                if (sum > m) {
+                    break;
+                }
+            }
+
+            if (sum == m) {
+                answer++;
+                p1++;
+                p2++;
+            } else if (sum > m) {
+                p1++;
+            } else {
+                p2++;
+            }
+        }
+
+        return answer;
+    }
+
+}
