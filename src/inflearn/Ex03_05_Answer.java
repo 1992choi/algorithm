f42ccb0982fa55bcf433514dca0a1b7b7a7ec63c
choi <younggeunn@naver.com>
2023-01-15 14:51:58 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Two pointers, Sliding window > 5. 연속된 자연수의 합

--- src/inflearn/Ex03_05_Answer.java
+++ src/inflearn/Ex03_05_Answer.java
@@ -8,10 +8,12 @@ public class Ex03_05_Answer {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        System.out.println(solution(n));
+        System.out.println(solution1(n));
+        System.out.println(solution2(n));
     }
 
-    public static int solution(int n) {
+    // two pointers 사용
+    public static int solution1(int n) {
         int answer = 0;
         int sum = 0;
         int lt = 0;
@@ -36,6 +38,23 @@ public class Ex03_05_Answer {
                 }
             }
 
+        }
+
+        return answer;
+    }
+
+    // 수학적 접근
+    public static int solution2(int n) {
+        int answer = 0;
+        int cnt = 1;
+        n--;
+        while (n > 0) {
+            cnt++;
+            n = n - cnt;
+
+            if (n % cnt == 0) {
+                answer++;
+            }
         }
 
         return answer;
