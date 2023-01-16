e797f16992bc0c1452adbf66a330e1bad8523f8d
choi <younggeunn@naver.com>
2023-01-16 20:55:20 +0900

    자바(Java) 알고리즘 문제풀이 입문 - HashMap, TreeSet > 1. 학급 회장(해쉬)

---
+++ src/inflearn/Ex04_01.java
@@ -0,0 +1,43 @@
+package inflearn;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Ex04_01 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String str = in.next();
+
+        System.out.println(solution(n, str));
+    }
+
+    public static String solution(int n, String str) {
+        String answer = "";
+        String[] arr = str.split("");
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int count = map.getOrDefault(arr[i], 0) + 1;
+            map.put(arr[i], count);
+        }
+
+        int max = Integer.MIN_VALUE;
+        Iterator it = map.keySet().iterator();
+        while (it.hasNext()) {
+            String key = it.next().toString();
+            int value = map.get(key);
+
+            if (value > max) {
+                max = value;
+                answer = key;
+            }
+        }
+
+        return answer;
+    }
+
+}
