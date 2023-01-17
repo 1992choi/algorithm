baf14c1163296eba748136a4ad7fcf8e5527f31b
choi <younggeunn@naver.com>
2023-01-17 21:31:19 +0900

    자바(Java) 알고리즘 문제풀이 입문 - HashMap, TreeSet > 3. 매출액의 종류

--- src/inflearn/Ex04_03.java
+++ src/inflearn/Ex04_03.java
@@ -1,8 +1,6 @@
 package inflearn;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Ex04_03 {
 
@@ -15,7 +13,8 @@ public class Ex04_03 {
             arr[i] = in.nextInt();
         }
 
-        solution(n, k, arr);
+        // solution(n, k, arr); // Time out
+        solution2(n, k, arr);
     }
 
     public static void solution(int n, int k, int[] arr) {
@@ -30,6 +29,33 @@ public class Ex04_03 {
             System.out.print(set.size() + " ");
             lt++;
             rt++;
+        }
+    }
+
+    public static void solution2(int n, int k, int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int lt = 0;
+        int rt = k;
+
+        // 초기 구성
+        for (int i = lt; i < rt; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        System.out.print(map.size() + " ");
+
+        // 슬라이드
+        for (int i = rt; i < n; i++) {
+            int leftValue = arr[i - k];
+            int rightValue = arr[i];
+
+            if (map.getOrDefault(leftValue, 0) <= 1) {
+                map.remove(leftValue);
+            } else {
+                map.put(leftValue, map.get(leftValue) - 1);
+            }
+            map.put(rightValue, map.getOrDefault(rightValue, 0) + 1);
+
+            System.out.print(map.size() + " ");
         }
     }
 
