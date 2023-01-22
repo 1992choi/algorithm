c989b1330ec7cb8dfc136308b6d4c0adbec67655
choi <younggeunn@naver.com>
2023-01-22 21:23:16 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Stack, Queue > 4. 후위식 연산(postfix)

---
+++ src/inflearn/Ex05_04_Answer.java
@@ -0,0 +1,40 @@
+package inflearn;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Ex05_04_Answer {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+
+        System.out.println(solution(s));
+    }
+
+    public static int solution(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (char x : s.toCharArray()) {
+            if (Character.isDigit(x)) {
+                stack.push(x - 48);
+            } else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+
+                if (x == '+') {
+                    stack.push(lt + rt);
+                } else if (x == '-') {
+                    stack.push(lt - rt);
+                } else if (x == '*') {
+                    stack.push(lt * rt);
+                } else if (x == '/') {
+                    stack.push(lt / rt);
+                }
+            }
+        }
+
+        return stack.get(0);
+    }
+
+}
