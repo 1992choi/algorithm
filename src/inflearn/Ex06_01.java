85f541ec3afd12e44a6efe85a24539982a6a0482
choi <younggeunn@naver.com>
2023-01-23 18:21:30 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Sorting and Searching > 1. 선택 정렬

---
+++ src/inflearn/Ex06_01.java
@@ -0,0 +1,35 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Ex06_01 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        solution(n, arr);
+    }
+
+    public static void solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[i]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        // print
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
