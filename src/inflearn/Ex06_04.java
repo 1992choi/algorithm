db45521569539d2045c85eb6e0e7852918a371e1
choi <younggeunn@naver.com>
2023-01-24 13:51:59 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Sorting and Searching > 4. Least Recently Used

---
+++ src/inflearn/Ex06_04.java
@@ -0,0 +1,36 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex06_04 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        solution(s, n, arr);
+    }
+
+    public static void solution(int s, int n, int[] arr) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (list.contains(arr[i])) {
+                list.remove(new Integer(arr[i]));
+            }
+            list.add(0, arr[i]);
+        }
+
+        for (int i = 0; i < s; i++) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+
+}
