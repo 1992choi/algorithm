abb5ccb98db288ec81dbff688978fcfd9bc2c141
choi <younggeunn@naver.com>
2023-01-24 14:12:11 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Sorting and Searching > 5. 중복 확인

---
+++ src/inflearn/Ex06_05_Answer.java
@@ -0,0 +1,31 @@
+package inflearn;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex06_05_Answer {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(solution(n, arr));
+    }
+
+    public static String solution(int n, int[] arr) {
+        String answer = "U";
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return "D";
+            }
+        }
+
+        return answer;
+    }
+
+}
