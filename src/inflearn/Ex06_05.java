57e675e69315c5fed5e2dac51e7fadd1c1c3bed1
choi <younggeunn@naver.com>
2023-01-24 14:08:04 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Sorting and Searching > 5. 중복 확인

---
+++ src/inflearn/Ex06_05.java
@@ -0,0 +1,29 @@
+package inflearn;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Ex06_05 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(solution(n, arr));
+    }
+
+    public static String solution(int n, int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(arr[i]);
+        }
+
+        return set.size() == n ? "U" : "D";
+    }
+
+}
