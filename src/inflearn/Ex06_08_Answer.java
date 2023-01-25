6d98a1694c59eae2fb05c83b3176baa490735003
choi <younggeunn@naver.com>
2023-01-25 21:52:38 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Sorting and Searching > 8. 이분검색

---
+++ src/inflearn/Ex06_08_Answer.java
@@ -0,0 +1,42 @@
+package inflearn;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex06_08_Answer {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        solution(n, m, arr);
+    }
+
+    public static void solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0;
+        int rt = n - 1;
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+            if (arr[mid] > m) {
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
