eac43ed344d186d99c208dc0f7428ce37ed5c528
choi <younggeunn@naver.com>
2023-01-25 21:25:43 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Sorting and Searching > 7. 좌표 정렬

---
+++ src/inflearn/Ex06_07_Answer.java
@@ -0,0 +1,55 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex06_07_Answer {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        List<Point> points = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            points.add(new Point(x, y));
+        }
+
+        Collections.sort(points);
+
+        // print
+        for (Point point : points) {
+            System.out.println(point.getX() + " " + point.getY());
+        }
+    }
+
+}
+
+class Point implements Comparable<Point> {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if(this.x == o.x) {
+            return this.y - o.y;
+        } else {
+            return this.x - o.x;
+        }
+    }
+}
