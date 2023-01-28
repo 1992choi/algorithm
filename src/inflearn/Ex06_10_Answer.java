189adf1e916c229ffd53d9a802d0e65db1634e3e
choi <younggeunn@naver.com>
2023-01-28 10:39:33 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Sorting and Searching > 10. 마구간 정하기(결정알고리즘)

---
+++ src/inflearn/Ex06_10_Answer.java
@@ -0,0 +1,53 @@
+package inflearn;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex06_10_Answer {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int c = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(solution(n, c, arr));
+    }
+
+    public static int solution(int n, int c, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n - 1];
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) >= c) {
+                answer = mid;
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static int count(int[] arr, int dist) {
+        int cnt = 1;
+        int ep = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+
+        return cnt;
+    }
+
+}
