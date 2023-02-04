844f8827ccae948d0f46fabad03b57d922763193
choi <younggeunn@naver.com>
2023-02-04 11:22:33 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Recursive, Tree, Graph > 10. Tree 말단노드까지의 가장 짧은 경로(BFS)

---
+++ src/inflearn/Ex07_10_Answer.java
@@ -0,0 +1,69 @@
+package inflearn;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Ex07_10_Answer {
+
+    public static void main(String[] args) {
+        /*
+            Ex07_05_Answer.java 파일에 선언되어 있음
+
+            class Node {
+
+                int data;
+                Node lt;
+                Node rt;
+
+                public Node(int val) {
+                    data = val;
+                    lt = null;
+                    rt = null;
+                }
+
+            }
+         */
+        Node root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+
+        /*
+            ========== 트리 형태 ==========
+                        1
+                  2           3
+               4     5
+         */
+
+        System.out.println(BFS(root));
+    }
+
+    public static int BFS(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Node currentNode = queue.poll();
+                if (currentNode.lt == null && currentNode.rt == null) {
+                    return level;
+                }
+
+                if (currentNode.lt != null) {
+                    queue.offer(currentNode.lt);
+                }
+                if (currentNode.rt != null) {
+                    queue.offer(currentNode.rt);
+                }
+            }
+            level++;
+        }
+
+        return -1;
+    }
+
+}
+
