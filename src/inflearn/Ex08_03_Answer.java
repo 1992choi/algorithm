9fec21ba34a62805e2cc0e7577d30be6e036b632
choi <younggeunn@naver.com>
2023-02-09 23:04:06 +0900

    자바(Java) 알고리즘 문제풀이 입문 - DFS, BFS 활용 > 3. 최대점수 구하기

--- src/inflearn/Ex08_03_Answer.java
+++ src/inflearn/Ex08_03_Answer.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Ex08_03_Answer {
 
-    static int[] pm;
+    static int answer = Integer.MIN_VALUE;
     static int n;
     static int m;
 
@@ -12,24 +12,28 @@ public class Ex08_03_Answer {
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
         m = in.nextInt();
-        pm = new int[m];
+        int[] a = new int[n]; // 점수
+        int[] b = new int[n]; // 걸리는 시간
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+            b[i] = in.nextInt();
+        }
 
-        DFS(0);
+        DFS(0, 0, 0, a, b);
+        System.out.println(answer);
     }
 
-    public static void DFS(int level) {
-        if (level == m) {
-            for (int x : pm) {
-                System.out.print(x + " ");
-            }
-            System.out.println();
+    public static void DFS(int level, int sum, int totalTime, int[] score, int[] time) {
+        if (totalTime > m) {
+            return;
+        }
+
+        if (level == n) {
+            answer = Math.max(answer, sum);
         } else {
-            for (int i = 1; i <= n; i++) {
-                pm[level] = i;
-                DFS(level + 1);
-            }
+            DFS(level + 1, sum + score[level], totalTime + time[level], score, time);
+            DFS(level + 1, sum, totalTime, score, time);
         }
     }
 
 }
-
