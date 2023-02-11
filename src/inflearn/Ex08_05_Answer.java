43c125ef9f8071ae81e6ad91332253fbab9f66a4
choi <younggeunn@naver.com>
2023-02-11 21:08:35 +0900

    자바(Java) 알고리즘 문제풀이 입문 - DFS, BFS 활용 > 5. 동전교환

---
+++ src/inflearn/Ex08_05_Answer.java
@@ -0,0 +1,42 @@
+package inflearn;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ex08_05_Answer {
+
+    static int n;
+    static int m;
+    static int answer = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        m = in.nextInt();
+
+        DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+
+    public static void DFS(int level, int sum, Integer[] arr) {
+        if (sum > m || answer <= level) {
+            return;
+        }
+
+        if (sum == m) {
+            answer = Math.min(level, answer);
+        } else {
+            for (int i = 0; i < n; i++) {
+                DFS(level + 1, sum + arr[i], arr);
+            }
+        }
+    }
+
+}
+
