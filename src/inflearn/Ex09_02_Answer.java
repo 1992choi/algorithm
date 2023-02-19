85d759a49a9c2786a72c35b74a6b7c80a390f40b
choi <younggeunn@naver.com>
2023-02-19 12:41:46 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Greedy Algorithm > 2. 회의실 배정

---
+++ src/inflearn/Ex09_02_Answer.java
@@ -0,0 +1,55 @@
+package inflearn;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Ex09_02_Answer {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        List<Time> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new Time(in.nextInt(), in.nextInt()));
+        }
+
+        System.out.println(solution(list, n));
+    }
+
+    static int solution(List<Time> list, int n) {
+        int cnt = 0;
+
+        Collections.sort(list);
+        int endTIme = 0;
+        for (Time time : list) {
+            if (time.s >= endTIme) {
+                cnt++;
+                endTIme = time.e;
+            }
+        }
+
+        return cnt;
+    }
+
+    static class Time implements Comparable<Time> {
+        int s;
+        int e;
+
+        Time(int s, int e) {
+            this.s = s;
+            this.e = e;
+        }
+
+        @Override
+        public int compareTo(Time o) {
+            if (this.e == o.e) {
+                return this.s - o.s;
+            } else {
+                return this.e - o.e;
+            }
+        }
+    }
+
+}
