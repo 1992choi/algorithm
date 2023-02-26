5d7a2f5a6e9e7285a88a2a787d2e5dead030847b
choi <younggeunn@naver.com>
2023-02-26 10:57:40 +0900

    자바(Java) 알고리즘 문제풀이 입문 - Dynamic Programming > 6. 최대점수 구하기(냅색알고리즘)

---
+++ src/inflearn/Ex10_06_Answer.java
@@ -0,0 +1,25 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class Ex10_06_Answer {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] dy = new int[m + 1];
+
+        for (int i = 0; i < n; i++) {
+            int score = kb.nextInt();
+            int time = kb.nextInt();
+
+            for (int j = m; j >= time; j--) {
+                dy[j] = Math.max(dy[j], dy[j - time] + score);
+            }
+        }
+
+        System.out.println(dy[m]);
+    }
+
+}
