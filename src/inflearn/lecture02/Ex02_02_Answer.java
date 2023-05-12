b0f008e01ab2543fc4772c2d7b968e5d35a546ff
choi <younggeunn@naver.com>
2023-05-12 21:09:33 +0900

    자바 코딩테스트(IT 대기업 최신 기출 반영) - 해싱 & 시간파싱 > 2. 같은 빈도수 만들기

---
+++ src/inflearn/lecture02/Ex02_02_Answer.java
@@ -0,0 +1,39 @@
+package inflearn.lecture02;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Ex02_02_Answer {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("aaabc"))); // [0, 2, 2, 3, 3]
+        System.out.println(Arrays.toString(solution("aabb"))); // [0, 0, 2, 2, 2]
+        System.out.println(Arrays.toString(solution("abcde"))); // [0, 0, 0, 0, 0]
+        System.out.println(Arrays.toString(solution("abcdeabc"))); // [0, 0, 0, 1, 1]
+        System.out.println(Arrays.toString(solution("abbccddee"))); // [1, 0, 0, 0, 0]
+    }
+
+    public static int[] solution(String s) {
+        int[] answer = new int[5];
+
+        HashMap<Character, Integer> sH = new HashMap<>();
+        for (char x : s.toCharArray()) {
+            sH.put(x, sH.getOrDefault(x, 0) + 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        String tmp = "abcde";
+        for (char key : tmp.toCharArray()) {
+            if (sH.getOrDefault(key, 0) > max) {
+                max = sH.getOrDefault(key, 0);
+            }
+        }
+
+        for (int i = 0; i < tmp.length(); i++) {
+            answer[i] = max - sH.getOrDefault(tmp.charAt(i), 0);
+        }
+
+        return answer;
+    }
+
+}
