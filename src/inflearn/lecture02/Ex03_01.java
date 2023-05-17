163b666b952c13c827e18661dea20c5c3ab9da5f
choi <younggeunn@naver.com>
2023-05-17 21:42:00 +0900

    자바 코딩테스트(IT 대기업 최신 기출 반영) - 자료구조 활용 > 1. 최대 길이 연속수열

---
+++ src/inflearn/lecture02/Ex03_01.java
@@ -0,0 +1,33 @@
+package inflearn.lecture02;
+
+import java.util.Arrays;
+
+public class Ex03_01 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{8, 1, 9, 3, 10, 2, 4, 0, 2, 3}));
+        System.out.println(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0}));
+        System.out.println(solution(new int[]{3, 3, 3, 3, 3, 3, 3, 3}));
+        System.out.println(solution(new int[]{-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1}));
+        System.out.println(solution(new int[]{-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7}));
+    }
+
+    public static int solution(int[] nums) {
+        int answer = 1;
+
+        // 중복 제거 후 정렬
+        int[] distinctSortedNums = Arrays.stream(nums).distinct().sorted().toArray();
+
+        // 기준이 되는 다음 수가 1씩 증가하는 수인지 확인
+        for (int i = 0; i < distinctSortedNums.length - 1; i++) {
+            if (distinctSortedNums[i + 1] - distinctSortedNums[i] == 1) {
+                answer++;
+            } else {
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+}
