cb43f0a4a3c83bdf0155de9017a8ecce64f10591
choi <younggeunn@naver.com>
2023-05-24 22:59:31 +0900

    자바 코딩테스트(IT 대기업 최신 기출 반영) - 자료구조 활용 > 6. 가장 많이 사용된 회의실

---
+++ src/inflearn/lecture02/Ex03_06_Answer.java
@@ -0,0 +1,47 @@
+package inflearn.lecture02;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.TreeSet;
+
+public class Ex03_06_Answer {
+
+    public static void main(String[] args) {
+        System.out.println(solution(2, new int[][]{{0, 5}, {2, 7}, {4, 5}, {7, 10}, {9, 12}})); // 0
+        System.out.println(solution(3, new int[][]{{3, 9}, {1, 10}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}})); // 0
+        System.out.println(solution(3, new int[][]{{1, 30}, {2, 15}, {3, 10}, {4, 12}, {6, 10}})); // 1
+        System.out.println(solution(4, new int[][]{{3, 20}, {1, 25}, {5, 8}, {10, 15}, {9, 14}, {12, 14}, {15, 20}})); // 2
+    }
+
+    public static int solution(int n, int[][] meetings) {
+        int answer = 0;
+        int[] res = new int[n];
+        PriorityQueue<int[]> ends = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        TreeSet<Integer> rooms = new TreeSet<>();
+        for (int i = 0; i < n; i++) rooms.add(i);
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
+
+        for (int[] m : meetings) {
+            while (!ends.isEmpty() && ends.peek()[0] <= m[0]) rooms.add(ends.poll()[1]);
+            if (!rooms.isEmpty()) {
+                int room = rooms.pollFirst();
+                res[room]++;
+                ends.add(new int[]{m[1], room});
+            } else {
+                int[] e = ends.poll();
+                res[e[1]]++;
+                ends.add(new int[]{e[0] + (m[1] - m[0]), e[1]});
+            }
+        }
+
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            if (res[i] > maxi) {
+                maxi = res[i];
+                answer = i;
+            }
+        }
+        return answer;
+    }
+
+}
