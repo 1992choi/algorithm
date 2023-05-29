180eb068750308e1a83dfa154145c6024325a7fb
choi <younggeunn@naver.com>
2023-05-29 15:03:07 +0900

    자바 코딩테스트(IT 대기업 최신 기출 반영) - Sorting & Thinking > 6. 멀티태스킹

--- src/inflearn/lecture02/Ex04_06.java
+++ src/inflearn/lecture02/Ex04_06.java
@@ -29,7 +29,9 @@ public class Ex04_06 {
             }
         }
 
-        // 정전이후 수행
+        // 정전이후 수행 : 마지막 수행한 인덱스의 다음 인덱스부터 순회하며 작업이 남이 있는지 파악
+        // Ex) 마지막 작업이 3번째 였으면, 4, 5, 6, ... 2번째로 탐색
+
         // System.out.println("새로 시작할 작업 인덱스 = " + lastIndex);
         // System.out.println(Arrays.toString(tasks));
         for (int i = 0; i < tasks.length; i++) {
