dfd3e608eb0648708bac5e11e9682cbc2a2e1e6a
choi <younggeunn@naver.com>
2023-06-02 21:01:57 +0900

    자바 코딩테스트(IT 대기업 최신 기출 반영) - greedy > 1. 침몰하는 타이타닉

---
+++ src/inflearn/lecture02/Ex05_01.java
@@ -0,0 +1,33 @@
+package inflearn.lecture02;
+
+import java.util.*;
+
+public class Ex05_01 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{90, 50, 70, 100, 60}, 140)); // 3
+        System.out.println(solution(new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90}, 100)); // 5
+        System.out.println(solution(new int[]{68, 72, 30, 105, 55, 115, 36, 67, 119, 111, 95, 24, 25, 80, 55, 85, 75, 83, 21, 81}, 120)); // 14
+    }
+
+    public static int solution(int[] nums, int m) {
+        Arrays.sort(nums);
+
+        int answer = 0;
+        int lt = 0;
+        int rt = nums.length - 1;
+
+        while (lt <= rt) {
+            if (nums[lt] + nums[rt] <= m) {
+                lt++;
+                rt--;
+            } else {
+                rt--;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+
+}
