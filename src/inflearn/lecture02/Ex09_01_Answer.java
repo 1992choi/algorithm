4a211786b49ee002fb863b49b39a5e259fd76689
choi <younggeunn@naver.com>
2023-07-03 20:49:42 +0900

    자바 코딩테스트(IT 대기업 최신 기출 반영) - 다이나믹 프로그래밍 > 1. 사탕 가게

---
+++ src/inflearn/lecture02/Ex09_01_Answer.java
@@ -0,0 +1,29 @@
+package inflearn.lecture02;
+
+import java.util.Scanner;
+
+public class Ex09_01_Answer {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        while (true) {
+            int n = kb.nextInt();
+            int m = (int) Math.round(kb.nextDouble() * 100);
+            if (n == 0 && m == 0) {
+                break;
+            }
+
+            int[] dy = new int[m + 1];
+            for (int i = 0; i < n; i++) {
+                int c = kb.nextInt();
+                int p = (int) Math.round(kb.nextDouble() * 100);
+                for (int j = p; j <= m; j++) {
+                    dy[j] = Math.max(dy[j], dy[j - p] + c);
+                }
+            }
+
+            System.out.println(dy[m]);
+        }
+    }
+
+}
