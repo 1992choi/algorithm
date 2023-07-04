ad4cbc8ef9c3b95aa2da1fc3b455e82172147bcf
choi <younggeunn@naver.com>
2023-07-04 21:09:11 +0900

    자바 코딩테스트(IT 대기업 최신 기출 반영) - 다이나믹 프로그래밍 > 2. 수도배관공사

---
+++ src/inflearn/lecture02/Ex09_02_Answer.java
@@ -0,0 +1,37 @@
+package inflearn.lecture02;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex09_02_Answer {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int D = kb.nextInt();
+        int P = kb.nextInt();
+        int[] dy = new int[D + 1];
+        for (int i = 0; i < P; i++) {
+            int L = kb.nextInt();
+            int C = kb.nextInt();
+            for (int j = D; j > L; j--) {
+                if (dy[j - L] == 0) {
+                    continue;
+                }
+                dy[j] = Math.max(dy[j], Math.min(dy[j - L], C));
+            }
+            dy[L] = Math.max(dy[L], C);
+        }
+
+        /*
+            - 입력예제
+                7 6
+                4 5
+                3 6
+                2 7
+                1 4
+                6 7
+                1 5
+         */
+        System.out.println(dy[D]);
+    }
+}
