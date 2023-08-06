39619f7ea2fe567dbfb5e8b4a4e4e3dfad525197
choi <younggeunn@naver.com>
2023-08-06 17:44:25 +0900

    자바(Java) 알고리즘 문제풀이 입문 - String(문자열) > 10. 가장 짧은 문자거리

--- src/inflearn/lecture01/ex01/Ex01_10_Answer.java
+++ src/inflearn/lecture01/ex01/Ex01_10_Answer.java
@@ -18,6 +18,7 @@ public class Ex01_10_Answer {
         char[] chars = str.toCharArray();
         int[] answer = new int[str.length()];
 
+        // 왼쪽부터 탐색. 자기 자신의 왼쪽에 있는 t문자와 비교
         int p = 100;
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == targetChar) {
@@ -29,6 +30,7 @@ public class Ex01_10_Answer {
             }
         }
 
+        // 오른쪽부터 탐색. 자기 자신의 오른쪽에 있는 t문자와 비교
         p = 100;
         for (int i = chars.length - 1; i >= 0; i--) {
             if (chars[i] == targetChar) {
