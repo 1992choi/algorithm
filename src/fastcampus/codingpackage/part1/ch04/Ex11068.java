3d57e78ceedbfc221347c05eed6a1e6fd49f1c33
choi <younggeunn@naver.com>
2025-09-18 21:54:48 +0900

    핵심유형 20개로 한 번에 끝내는 알고리즘 코딩테스트 with Java 초격차 패키지 Online

--- src/fastcampus/codingpackage/part1/ch04/Ex11068.java
+++ src/fastcampus/codingpackage/part1/ch04/Ex11068.java
@@ -42,3 +42,44 @@ public class Ex11068 {
     }
 
 }
+
+/*
+    풀이
+    - 64진법에서는 숫자, 소문자, 대문자를 모두 사용하여도 62번째와 63번째가 남아서 별도의 문자가 필요하다.
+      하지만, 회문인 수의 특징을 생각해보면 문자열을 매핑하지 않아도 된다.
+        - 대칭적인 특징을 이용하여 digit[i]과 digit[len - i - 1] 이 같은지만 확인하면 된다.
+      진법을 표시할 때, 10진수의 자릿값을 그대로 사용하면 된다.
+        - 64진법이라고 가정할 때, 각 자리를 아래와 같이 분리할 수 있다.
+          (11 * 63의 3승) + (62 * 63의 2승) + (62 * 63의 1승) + (11 * 63의 0승)
+          --> 11, 62, 62, 11 만 가지고 회문인 수인지 판단가능.
+
+    public static boolean isPalindromeInBase(int x, int base) {
+        int[] digit = new int[20];
+        int len = 0;
+        while (x > 0) {
+            digit[len++] = x % base;
+            x /= base;
+        }
+
+        for (int i = 0; i < len / 2; i++)
+            if (digit[i] != digit[len - i - 1])
+                return false;
+        return true;
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int x = sc.nextInt();
+            boolean ans = false;
+            for (int i = 2; i <= 64; i++) {
+                if (isPalindromeInBase(x, i)) {
+                    ans = true;
+                    break;
+                }
+            }
+            System.out.println(ans ? 1 : 0);
+        }
+    }
+ */
