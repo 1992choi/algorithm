ec274bc71de19f279428b42f0ccefefc73538140
choi <younggeunn@naver.com>
2025-09-18 21:17:49 +0900

    핵심유형 20개로 한 번에 끝내는 알고리즘 코딩테스트 with Java 초격차 패키지 Online

--- src/fastcampus/codingpackage/part1/ch04/Ex11005.java
+++ src/fastcampus/codingpackage/part1/ch04/Ex11005.java
@@ -14,3 +14,21 @@ public class Ex11005 {
     }
 
 }
+
+/*
+    풀이
+
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int B = sc.nextInt();
+
+    String ans = "";
+    while (N > 0) {
+        int digit = N % B;
+        if (digit < 10) ans += digit;
+        else ans += (char)('A' + digit - 10);
+        N /= B;
+    }
+
+    System.out.println(new StringBuilder(ans).reverse());
+ */
