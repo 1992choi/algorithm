d64d81083a7175db706b5a40f87e100fa57a6916
choi <younggeunn@naver.com>
2025-09-26 21:25:53 +0900

    핵심유형 20개로 한 번에 끝내는 알고리즘 코딩테스트 with Java 초격차 패키지 Online

--- src/fastcampus/codingpackage/part1/ch05/Ex1181.java
+++ src/fastcampus/codingpackage/part1/ch05/Ex1181.java
@@ -35,3 +35,30 @@ public class Ex1181 {
     }
 
 }
+
+/*
+    풀이
+    - 내 풀이와 다른점
+      - return o1.compareTo(o2); : 문자열은 사전순 정렬을 해주기 때문에 charAt으로 한 글자씩 비교할 필요가 없다.
+
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    String[] words = new String[N];
+    for (int i = 0; i < N; i++)
+        words[i] = sc.next();
+
+    Arrays.sort(words, new Comparator<String>() {
+        @Override
+        public int compare(String o1, String o2) {
+            if (o1.length() == o2.length())
+                return o1.compareTo(o2);
+            return o1.length() - o2.length();
+        }
+    });
+
+    System.out.println(words[0]);
+    for (int i = 1; i < N; i++)
+        if (!words[i].equals(words[i - 1]))
+            System.out.println(words[i]);
+ */
