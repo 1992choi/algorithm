0c9d32a857041c34af0a5f1e14946e9565f3ef42
choi <younggeunn@naver.com>
2025-09-29 21:39:46 +0900

    핵심유형 20개로 한 번에 끝내는 알고리즘 코딩테스트 with Java 초격차 패키지 Online

--- src/fastcampus/codingpackage/part1/ch05/Ex1302.java
+++ src/fastcampus/codingpackage/part1/ch05/Ex1302.java
@@ -32,3 +32,34 @@ public class Ex1302 {
     }
 
 }
+
+/*
+    풀이
+    - 책 제목을 정렬한 후 카운트를 센다.
+      - 이미 오름차순으로 정렬되어 있으므로 동일 카운트에서는 사전 순으로 가장 앞서는 제목이 무엇인지 추가로 찾을 필요가 없다.
+      - 정렬이 되었으므로, 동일한 제목의 책이 연속적으로 있기 때문에 누적 카운트를 활용하여 최댓값을 쉽게 구할 수 있다.
+
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    String[] titles = new String[N];
+    for (int i = 0; i < N; i++)
+        titles[i] = sc.next();
+
+    Arrays.sort(titles);
+
+    String maxTitle = titles[0];
+    int maxCount = 1;
+    int currentCount = 1;
+    for (int i = 1; i < N; i++) {
+        if (!titles[i].equals(titles[i - 1]))
+            currentCount = 0;
+        currentCount++;
+        if (maxCount < currentCount ||
+                (maxCount == currentCount && titles[i].compareTo(maxTitle) < 0)) {
+            maxCount = currentCount;
+            maxTitle = titles[i];
+        }
+    }
+    System.out.println(maxTitle);
+ */
--- src/fastcampus/codingpackage/part1/ch05/Ex7785.java
+++ src/fastcampus/codingpackage/part1/ch05/Ex7785.java
@@ -24,3 +24,33 @@ public class Ex7785 {
     }
 
 }
+
+/*
+    풀이
+    - 이름으로 먼저 정렬을 한 후, 조건만 판단하면 간단히 풀 수 있다.
+      - 조건 : 현재 레코드가 "enter" 상태이면서, 다음 레코드의 이름과 현재 이름이 다른 경우
+        --> 마지막 액션이 enter면 회사에 남아있는 사람이다. (이와 같이 조건을 단순화하여 풀 수 있다.)
+
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    String[][] records = new String[N][2];
+    for (int i = 0; i < N; i++) {
+        records[i][0] = sc.next();
+        records[i][1] = sc.next();
+    }
+
+    Arrays.sort(records, new Comparator<String[]>() {
+        @Override
+        public int compare(String[] o1, String[] o2) {
+            return o2[0].compareTo(o1[0]);
+        }
+    });
+
+    for (int i = 0; i < N - 1; i++) {
+        if (records[i][1].equals("enter") && !records[i][0].equals(records[i + 1][0]))
+            System.out.println(records[i][0]);
+    }
+    if (records[N - 1][1].equals("enter"))
+        System.out.println(records[N - 1][0]);
+ */
