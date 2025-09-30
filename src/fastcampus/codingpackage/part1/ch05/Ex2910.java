2d6b58ffb6cc18bfa229de1e5e39a273800d5c61
choi <younggeunn@naver.com>
2025-09-30 11:58:27 +0900

    핵심유형 20개로 한 번에 끝내는 알고리즘 코딩테스트 with Java 초격차 패키지 Online

--- src/fastcampus/codingpackage/part1/ch05/Ex2910.java
+++ src/fastcampus/codingpackage/part1/ch05/Ex2910.java
@@ -54,3 +54,36 @@ public class Ex2910 {
     }
 
 }
+
+/*
+    풀이
+    - 'key=숫자 / value=빈도수'인 맵을 만들어서 정렬에 이용한다.
+      이때, 최초 등장한 index를 관리해야하는데, 빈도수와 index를 갖는 객체를 만들수도 있지만, LinkedHashMap의 특성을 이용하면 index는 관리할 필요가 없다.
+      LinkedHashMap은 만들어진 순서가 보장되며, 이를 'Integer[] frequencies'로 만들고 정렬하면 Integer도 Tim sort에 의해 순서가 보장된다.
+
+
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    int C = sc.nextInt();
+    Map<Integer, Integer> messages = new LinkedHashMap<>();
+    for (int i = 0; i < N; i++) {
+        int message = sc.nextInt();
+        messages.put(message, messages.getOrDefault(message, 0) + 1);
+    }
+
+    Integer[] frequencies = messages.keySet().toArray(new Integer[messages.size()]);
+    Arrays.sort(frequencies, new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            return messages.get(o2) - messages.get(o1);
+        }
+    });
+
+    for (int frequency : frequencies) {
+        int count = messages.get(frequency);
+        while (count-- > 0)
+            System.out.print(frequency + " ");
+    }
+    System.out.println();
+ */
