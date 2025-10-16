05b75276b0fcd34844582852659aa761900d4b62
choi <younggeunn@naver.com>
2025-10-16 21:50:54 +0900

    핵심유형 20개로 한 번에 끝내는 알고리즘 코딩테스트 with Java 초격차 패키지 Online

---
+++ src/fastcampus/codingpackage/part1/ch07/Ex10816.java
@@ -0,0 +1,36 @@
+package fastcampus.codingpackage.part1.ch07;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Ex10816 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            bw.write(map.getOrDefault(num, 0) + " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
