5f436477b8b64844a24120aba1b87f53c73364f1
choi <younggeunn@naver.com>
2025-10-21 22:52:34 +0900

    핵심유형 20개로 한 번에 끝내는 알고리즘 코딩테스트 with Java 초격차 패키지 Online

--- src/fastcampus/codingpackage/part1/ch07/Ex1654.java
+++ src/fastcampus/codingpackage/part1/ch07/Ex1654.java
@@ -37,3 +37,38 @@ public class Ex1654 {
     }
 
 }
+
+/*
+    풀이
+    - 시간초과를 해결하기 위하여 이분탐색으로 접근을 했어야함.
+
+
+    static int calculateCount(int[] lengths, long cutLength) {
+        int cnt = 0;
+        for (int l : lengths)
+            cnt += l / cutLength;
+        return cnt;
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int K = sc.nextInt();
+        int N = sc.nextInt();
+        int[] len = new int[K];
+        for (int i = 0; i < K; i++)
+            len[i] = sc.nextInt();
+
+        long l = 1, r = (1L << 31) - 1, ans = -1;
+        while (l <= r) {
+            long m = l + (r - l) / 2;
+            if (calculateCount(len, m) >= N) { // 해당 길이의 랜선을 N개 이상 만들 수 있다면 길이를 늘려본다.
+                ans = m;
+                l = m + 1;
+            } else { // N개 이상 만들 수 없다면 길이를 줄여본다.
+                r = m - 1;
+            }
+        }
+        System.out.println(ans);
+    }
+ */
